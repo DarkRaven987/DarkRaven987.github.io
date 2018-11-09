0f43a57f774e5f677882e8eba079ab2fb3e497c5
DarkRaven987 <darkraven987@gmail.com>
2018-11-09 16:59:43 +0200

    Lesson 6 hw worked for bit

--- Lesson_06/Homework_01/app.js
+++ Lesson_06/Homework_01/app.js
@@ -26,6 +26,39 @@ let products = [
   var pagination_buttons = document.getElementsByClassName("page-link");
   let search_textbox = document.getElementById("s");
 
+  function createAddMenu(){
+	  var addMenuBody = document.getElementById("add_menu");
+	  var title = createNewElement("h3","Create item menu",null);
+
+	  var nameRow = createNewElement("div",null,"row");
+	  var name_text = createNewElement("h4","Item name",null);
+	  var name_input = createNewElement("input",null,"form-control mr-sm-2",[{"name":"id", "value":"new_name"},{"name":"type","value":"text"},{"name":"placeholder","value":"Item name"},{"name":"aria-label","value":"Item name"}]);
+	  nameRow = attachChildrenToParent(createNewElement("div",null,"row"),[name_text, name_input]);
+
+	  var priceRow = createNewElement("div",null,"row");
+	  var price_text = createNewElement("h4","Item price",null);
+	  var price_input = createNewElement("input",null,"form-control mr-sm-2",[{"name":"id", "value":"new_price"},{"name":"type","value":"number"},{"name":"placeholder","value":"Item price"},{"name":"aria-label","value":"Item price"}]);
+	  priceRow = attachChildrenToParent(createNewElement("div",null,"row"),[price_text, price_input]);
+
+	  var imageRow = createNewElement("div",null,"row");
+	  var image_text = createNewElement("h4","Image",null);
+	  var image_input = createNewElement("input",null,"form-control mr-sm-2",[{"name":"id", "value":"new_image"},{"name":"type","value":"URL"},{"name":"placeholder","value":"Image URL"},{"name":"aria-label","value":"Image URL"}]);
+	  imageRow = attachChildrenToParent(createNewElement("div",null,"row"),[image_text, image_input]);
+
+	  var descRow = createNewElement("div",null,"row");
+	  var desc_text = createNewElement("h4","Description",null);
+	  var desc_input = createNewElement("textarea",null,"form-control mr-sm-2",[{"name":"id", "value":"new_desc"},{"name":"cols","value":"40"},{"name":"rows","value":"5"}]);
+	  descRow = attachChildrenToParent(createNewElement("div",null,"row"),[desc_text, desc_input]);
+
+	  var addBtn = createNewElement("button","Add item","btn",[{"name":"id","value":"addItemBtn"}]);
+
+	 var addMenu = createNewElement("div",null,"my-3");
+	 addMenu = attachChildrenToParent(createNewElement("div",null,"my-3"),[title,nameRow,priceRow,imageRow,descRow,addBtn])
+	
+	addMenuBody.appendChild(addMenu);
+	return addMenuBody;	
+  }
+
 function createPagination(array_products, index){
 	var pagination = document.getElementById("pagination");
 	pagination.innerHTML="";
@@ -38,9 +71,9 @@ function createPagination(array_products, index){
 	for( let i=0;i<Math.ceil(array_products.length/per_page);i++){
 		link = createNewElement("a", i+1, "page-link",[{"name":"href", "value":"#"},{"name":"data-link", "value": i}]);
 		if(i===index){
-			li=attachChilderToParent(createNewElement("li",null,"page-item active"),[link]);
+			li=attachChildrenToParent(createNewElement("li",null,"page-item active"),[link]);
 		}else{
-		li=attachChilderToParent(createNewElement("li",null,"page-item"),[link]);
+		li=attachChildrenToParent(createNewElement("li",null,"page-item"),[link]);
 		}
 		ul.appendChild(li);
 	}
@@ -56,7 +89,6 @@ function createPagination(array_products, index){
 	});
 }
 
-
 function createNewElement(tag, innerContent=null, classStr=null, attr=null){
 	var el = document.createElement(tag);
 	el.innerHTML = (innerContent)?innerContent:"";
@@ -70,22 +102,27 @@ function createNewElement(tag, innerContent=null, classStr=null, attr=null){
 }
 
 function createCard(product){
-	var link = createNewElement("a", "Add to cart", "btn btn-primary",[{"name":"href", "value":"#"}]);
+	var link = createNewElement("a", "Add to cart", "btn btn-primary", [{"name":"href", "value":"#"}]);
 	var p = createNewElement("p", product.description, "card-text");
 	var title = createNewElement("h5", product.name, "card-title");
+	var card_price = createNewElement("h3","$"+product.price,"card-price");
+	
+	var card_body = createNewElement("div", null, "card-body");
 
-	var cardBody = attachChilderToParent(createNewElement("div", null, "card-body"), [title,p,link]);
+	card_body = attachChildrenToParent(createNewElement("div",null,"card-body"),[title,p,card_price,link]);
 
-	var image = createNewElement("img", null, "card-img-top", [{"name":"src", "value":"images/"+product.image},{"name":"alt", "value":product.name}]);
+	var productImage = createNewElement("img", null,"card-img-top", [{"name": "src", "value": "./images/"+product.image},{"name": "alt", "value": product.name}]);
 
-	var card = attachChilderToParent(createNewElement("div", null, "card"), [image,cardBody]);
+	var wholeCard = createNewElement("div", null,"card");
+	wholeCard = attachChildrenToParent(createNewElement("div",null,"card"),[productImage, card_body]);
+	
+	var cardContainer = createNewElement("div", null,".col-lg-3 col-md-6 mb-2 catalog-item");
+	cardContainer = attachChildrenToParent(createNewElement("div", null,".col-lg-3 col-md-6 mb-2 catalog-item"),[wholeCard]);
 
-	var catalogItem = attachChilderToParent(createNewElement("div", null, "col-lg-3 col-md-6 mb-2 catalog-item"), [card]);
-
-	return catalogItem;
+	return cardContainer;
 }
 
-function attachChilderToParent(html, array_el){
+function attachChildrenToParent(html, array_el){
 	array_el.map((el)=>html.appendChild(el));
 	return html;
 }
@@ -110,7 +147,7 @@ function filterAndRenderProducts() {
 	let search_products = [];
 	let s = document.getElementById("s").value;
 	const reg = RegExp(s);
-	search_products = products.filter((el)=>reg.test(el.description));
+	search_products = products.filter((el)=>reg.test(el.name));
 	current_page=0;
 	renderProducts(search_products);
 	createPagination(search_products);
@@ -128,40 +165,10 @@ search_textbox.addEventListener("keypress", (e) =>{
 
 renderProducts(products);
 createPagination(products,0);
+createAddMenu();
 
 this.addEventListener("keypress", (e)=>{
-	/*console.log(e.keyCode);
-	if(e.keyCode==122){
+	if(e.keyCode == 122){
 		console.log("Message will be closed");
-	}*/
+	}
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// *.search("") - поиск фрагмента в стринге
-/*
-var str = "hello there"
-undefined
-str.match("ll");
-["ll", index: 2, input: "hello there", groups: undefined]0: "ll"groups: undefinedindex: 2input: "hello there"length: 1__proto__: Array(0)]
-str.match(/ll/g);
-["ll"]
-*/
